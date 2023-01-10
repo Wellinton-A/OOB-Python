2e45d6198acdc6682c5c1b2c5df699c13bae5838
wellinton <wellinton1067@gmail.com>
2023-01-10 19:42:35 -0300

    add csv file

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
+import csv
+
 class Item:
     rate_disc = 0.8
 
     all = []
+
     def __init__(self, name: str, price: float, quantity: int):
         self.name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
-    
-    def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
     def sold_item(self, n: int):
         self.quantity = self.quantity - n
-    
+
     def add_item(self, n: int):
         self.quantity = self.quantity + n
 
     def total_value_items(self):
         return self.quantity * self.price
 
-item1 =Item('Laptop', 999.90, 50)
-item2 =Item('Keyboard', 47.80, 150)
-item3 =Item('Mouse', 25.99, 100)
-item4 =Item('TV', 749.50, 25)
-item5 =Item('PS5', 1499.90, 10)
-item6 =Item('Monitor', 999.90, 30)
-item7 =Item('Ball', 5.9, 500)
-item8 =Item('Smartphone', 1050.99, 50)
-item9 =Item('CPU', 1350.79, 10)
-item10 =Item('RAM Memory', 250, 850)
-item11 =Item('Smartwatch', 500.80, 450)
-item12 =Item('Dog', 1875.89, 550)
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
 
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
+Item.instantiate_from_csv()
+
+# print(Item.all)
 dict_items = {}
+
 
 def dict_all_items():
     for i in Item.all:
         dict_items.update({i.name: i.price})
 
+
 dict_all_items()
+
 
 def max_value_item():
     key_list = list(dict_items.keys())
@@ -49,4 +59,3 @@ def max_value_item():
     return max_value
 
 print(max_value_item())
-    
